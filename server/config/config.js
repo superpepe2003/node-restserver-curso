27aae579f0c7ef448c79b0d6af26bdd4aef8389b
Pablo Fretes <pablo_fretes@hotmail.com>
2020-05-17 19:44:58 -0300

    Seguridad URI

--- server/config/config.js
+++ server/config/config.js
@@ -21,8 +21,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-
-    urlDB = 'mongodb+srv://strider:m1ldUeYSEtsEAMUu@cluster0-dt7xb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
